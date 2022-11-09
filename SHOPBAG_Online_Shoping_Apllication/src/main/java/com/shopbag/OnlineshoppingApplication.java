a60dd72f6df8d240dcb19d63d662a5a3d8adf677
globalmamtamishra <101391863+globalmamtamishra@users.noreply.github.com>
2022-11-09 21:03:41 +0530

    Add files via upload

---
+++ SHOPBAG_Online_Shoping_Apllication/src/main/java/com/shopbag/OnlineshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.shopbag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshoppingApplication.class, args);
+	}
+
+}
