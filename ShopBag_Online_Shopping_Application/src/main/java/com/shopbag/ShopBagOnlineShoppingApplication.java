f3a4460959321f0db8412caa1e98347e2848ff95
Siraj135 <shaiksirajuddin135@gmail.com>
2022-11-09 14:14:12 +0530

    validator added

--- ShopBag_Online_Shopping_Application/src/main/java/com/shopbag/ShopBagOnlineShoppingApplication.java
+++ ShopBag_Online_Shopping_Application/src/main/java/com/shopbag/ShopBagOnlineShoppingApplication.java
@@ -2,6 +2,9 @@ package com.shopbag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class ShopBagOnlineShoppingApplication {
@@ -10,4 +13,11 @@ public class ShopBagOnlineShoppingApplication {
 		SpringApplication.run(ShopBagOnlineShoppingApplication.class, args);
 	}
 
+	@Bean
+	public LocalValidatorFactoryBean validator(MessageSource ms) {
+		LocalValidatorFactoryBean lvfb = new LocalValidatorFactoryBean();
+		lvfb.setValidationMessageSource(ms);
+		
+		return lvfb;
+	}
 }
