a28aaca100b694e028a604f88069515596f8fa07
Siraj135 <shaiksirajuddin135@gmail.com>
2022-11-09 14:04:44 +0530

    created basic project structure

---
+++ ShopBag_Online_Shopping_Application/src/main/java/com/shopbag/ShopBagOnlineShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.shopbag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBagOnlineShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBagOnlineShoppingApplication.class, args);
+	}
+
+}
---
+++ ShopBag_Online_Shopping_Application/src/test/java/com/shopbag/ShopBagOnlineShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopbag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBagOnlineShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
