377e9bfea85f0531ae3ba988a9e3181a36aa6db4
Siraj135 <shaiksirajuddin135@gmail.com>
2022-11-10 16:32:17 +0530

    day-2

--- ShopBag_Online_Shopping_Application/src/main/java/com/shopbag/model/Order.java
+++ ShopBag_Online_Shopping_Application/src/main/java/com/shopbag/model/Order.java
@@ -16,6 +16,73 @@ public class Order {
 	private LocalDate orderDate;
 	private String orderStatus;
 	private Customer cutomer;
-	private Map productList;
+	private Map<Integer, Product> productList;
 	private Address address;
+	
+	public Order() {}
+
+	public Order(String orderId, LocalDate orderDate, String orderStatus, Customer cutomer,
+			Map<Integer, Product> productList, Address address) {
+		super();
+		this.orderId = orderId;
+		this.orderDate = orderDate;
+		this.orderStatus = orderStatus;
+		this.cutomer = cutomer;
+		this.productList = productList;
+		this.address = address;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public LocalDate getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDate orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public Customer getCutomer() {
+		return cutomer;
+	}
+
+	public void setCutomer(Customer cutomer) {
+		this.cutomer = cutomer;
+	}
+
+	public Map<Integer, Product> getProductList() {
+		return productList;
+	}
+
+	public void setProductList(Map<Integer, Product> productList) {
+		this.productList = productList;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [orderId=" + orderId + ", orderDate=" + orderDate + ", orderStatus=" + orderStatus + ", cutomer="
+				+ cutomer + ", productList=" + productList + ", address=" + address + "]";
+	}
 }
