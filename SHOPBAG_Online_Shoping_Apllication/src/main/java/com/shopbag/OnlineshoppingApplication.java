f2f388090205031671ff4f8f10a77466947b31bf
globalmamtamishra <101391863+globalmamtamishra@users.noreply.github.com>
2022-11-12 12:52:09 +0530

    Update OnlineshoppingApplication.java

--- SHOPBAG_Online_Shoping_Apllication/src/main/java/com/shopbag/OnlineshoppingApplication.java
+++ SHOPBAG_Online_Shoping_Apllication/src/main/java/com/shopbag/OnlineshoppingApplication.java
@@ -6,7 +6,11 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class OnlineshoppingApplication {
 
 	public static void main(String[] args) {
